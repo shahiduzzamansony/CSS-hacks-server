f49e71114117265309e5cc1ae59c0852897d8ded
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-10-26 22:55:46 +0600

    server site created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const app= express();
+const cors =require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const frameworks = require('./data/frameworks.json')
+
+app.get('/', (req,res) => {
+    res.send('news api running');
+});
+
+app.get('/frameworks',(req, res)=>[
+    res.send(frameworks)
+]);
+
+app.listen(port, ()=>{
+    console.log('site',port);
+})
