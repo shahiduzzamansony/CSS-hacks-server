2e81658c82dacef3793950489c544e843037374d
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-10-27 18:34:28 +0600

    details appi created

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require ('express')
-const app= express();
-const cors =require('cors');
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const frameworks = require('./data/frameworks.json')
+const frameworks = require("./data/frameworks.json");
+const informations = require("./data/frameworks.json");
 
-app.get('/', (req,res) => {
-    res.send('news api running ok');
+app.get("/", (req, res) => {
+  res.send("news api running ok");
 });
 
-app.get('/frameworks',(req, res)=>[
-    res.send(frameworks)
-]);
+app.get("/frameworks", (req, res) => [res.send(frameworks)]);
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedInfo = informations.find((n) => n.id === id);
+  res.send(selectedInfo);
+});
 
-app.listen(port, ()=>{
-    console.log('site',port);
-})
+app.listen(port, () => {
+  console.log("site", port);
+});
